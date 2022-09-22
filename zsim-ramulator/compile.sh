aedbd369fb3cc52a95d0e71d044f6f1e4ed614fc
Thilina Ratnayaka <thilinarmtb@gmail.com>
2022-09-22 15:26:58 +0000

    Untrack zsim-ramulator/libconfig

--- zsim-ramulator/compile.sh
+++ zsim-ramulator/compile.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 ZSIMPATH=$(pwd)
 PINPATH="$ZSIMPATH/pin"
-LIBCONFIGPATH="$ZSIMPATH/libconfig"
+LIBCONFIGPATH="${CONDA_PREFIX}"
 NUMCPUS=$(grep -c ^processor /proc/cpuinfo)
 
-
-
-if [ "$1" = "z" ]
-then
-	echo "Compiling only ZSim ..."
-        export PINPATH
-        export LIBCONFIGPATH
-        scons -j$NUMCPUS
-else
-	echo "Compiling all ..."
-	export LIBCONFIGPATH
-	cd $LIBCONFIGPATH
-	./configure --prefix=$LIBCONFIGPATH && make install
-	cd ..
-
-	export PINPATH
-	scons -j$NUMCPUS
-fi
+echo "Compiling only ZSim ..."
+export PINPATH
+export LIBCONFIGPATH
+scons -j$NUMCPUS
